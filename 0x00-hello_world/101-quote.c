8336d5ea3e070a0ba92ec704ac18e74c63a488c5
George_Amoasi_Fynn <georgefynn@gmail.com>
2023-02-21 08:31:48 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+*main - A program that print a line to the standard
+*Return: 1(success)
+*/
+
+int main(void)
+{
+	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
