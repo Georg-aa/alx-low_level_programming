53f29a34b67c5e68fb61aa3672846b65eeec86ef
George_Amoasi_Fynn <georgefynn@gmail.com>
2023-02-27 07:42:38 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,7 +20,7 @@ int main(void)
 
 	if (lastd > 5)
 	{
-	print("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	}
