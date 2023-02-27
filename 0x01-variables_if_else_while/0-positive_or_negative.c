734b0cd49af49c4eff9681e1778108a0b6aa8c29
George_Amoasi_Fynn <georgefynn@gmail.com>
2023-02-27 07:34:25 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,17 +13,18 @@ int main(void)
 
 {
 	int n, lastd;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
 
 	if (lastd > 5)
 	{
-	print("Last digit of %d is 5d and is greater than 5\n", n, lastd);
+	print("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	}
-	printf("last digit of %d is %d is %d and is 0\n", n, lastd);
+	printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
